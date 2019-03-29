84369f9b95d2ee8fd5670516bdd1448fc09dbef0
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-29 10:35:30 +0000

    Adicionar comentários

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,15 +4,20 @@ namespace MyFirstMethod
 {
     class Program
     {
+        //declarar método CountTo10
         static void CountTo10()
         {
+            //ciclo for que mostra o número da Count
             for(int i = 1; i <= 10; i++)
             {
+                //mostrar o número da count atual
                 Console.WriteLine(i);
             }
         }
+        
         static void Main(string[] args)
         {
+            //invocar método CountTo10 duas vezes
             CountTo10();
             CountTo10();
         }
