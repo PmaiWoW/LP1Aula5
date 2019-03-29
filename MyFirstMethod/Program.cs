1a829b97f086bf58af5b638ad42816e9183b0de6
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-29 11:40:11 +0000

    Add XML documentation

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,8 +4,10 @@ namespace MyFirstMethod
 {
     class Program
     {
-
-        //declarar método CountTo10
+        /// <summary>
+        /// Prints numbers from 1 to 10, using a simple for cicle
+        /// </summary>
+        /// <returns>Does not return anything</returns>
         static void CountTo10()
         {
             //ciclo for que mostra o número da Count
@@ -16,7 +18,13 @@ namespace MyFirstMethod
             }
         }
 
-
+        /// <summary>
+        /// Prints number from 1 to n, n being a variable that can be altered
+        /// when calling the method
+        /// </summary>
+        /// <param name="n">Variable used to determine the upper limit of the
+        /// count</param>
+        /// <returns>Does not return anything</returns>
         //declarar método CountToN, dando o argumento n, que determina a count
         static void CountToN(int n)
         {
@@ -28,6 +36,12 @@ namespace MyFirstMethod
             }
         }
         
+        /// <summary>
+        /// Core code inserted here, in this case the previous methods are 
+        /// called inside Main
+        /// </summary>
+        /// <param name="args">Arguments given in the console</param>
+        /// <returns>Does not return anything</returns>
         static void Main(string[] args)
         {
 
