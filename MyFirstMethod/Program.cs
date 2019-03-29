fcb784f797053e340c6be35fad02e79bbde67780
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-29 09:43:43 +0000

    Adicionar programa MyFirstMethod que cria um método que conta até dez e o invoca duas vezes no Main

---
+++ MyFirstMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstMethod
+{
+    class Program
+    {
+        static void CountTo10()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            CountTo10();
+            CountTo10();
+        }
+    }
+}
