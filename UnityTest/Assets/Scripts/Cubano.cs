f362f10b54f24ccfdabf38e51ffc8539ac592c3a
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-29 12:19:12 +0000

    Add unity project UnityTest

---
+++ UnityTest/Assets/Scripts/Cubano.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubano : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Charuto Cubano, rapaz");
+        transform.localScale = new Vector3(3, 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = new Vector3(0.5f, 0.5f, 0.5f);
+
+        transform.Rotate(rot);
+    }
+}
